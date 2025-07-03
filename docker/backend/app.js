c9792445ce2cc38730335d26f418f133d8900d74
Rajeshgupta123456789 <32204956+Rajeshgupta123456789@users.noreply.github.com>
2025-07-03 14:36:56 -0400

    Update app.js

--- docker/backend/app.js
+++ docker/backend/app.js
@@ -1,5 +1,11 @@
-
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Backend running'));
-app.listen(3000, () => console.log('App listening on port 3000'));
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the VideoTube Backend API!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
