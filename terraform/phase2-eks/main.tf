3cda639548a43c6e126cb7bab43c2eb5c095be08
Rajeshgupta123456789 <32204956+Rajeshgupta123456789@users.noreply.github.com>
2025-07-03 14:11:14 -0400

    Update main.tf

--- terraform/phase2-eks/main.tf
+++ terraform/phase2-eks/main.tf
@@ -1,11 +1,41 @@
+terraform {
+  required_version = ">= 1.4.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "local" {}
+}
+
+provider "aws" {
+  region = var.region
+}
 
 module "vpc" {
   source = "./vpc"
+  region = var.region
+}
+
+module "iam" {
+  source = "./iam"
 }
 
 module "eks" {
-  source         = "./eks"
-  cluster_name   = "my-eks-cluster"
-  cluster_role_arn = aws_iam_role.eks_cluster.arn
-  subnet_ids     = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+  source             = "./eks-cluster"
+  vpc_id             = module.vpc.vpc_id
+  public_subnet_ids  = module.vpc.public_subnet_ids
+  cluster_role_arn   = module.iam.eks_cluster_role_arn
+}
+
+module "nodegroup" {
+  source         = "./node-group"
+  cluster_name   = module.eks.cluster_name
+  node_role_arn  = module.iam.eks_node_role_arn
+  subnet_ids     = module.vpc.public_subnet_ids
+  ami_type       = "BOTTLEROCKET_x86_64"
+  security_group_ids  = [module.vpc.security_group_id]
+  key_name = "id_rsa"
 }
