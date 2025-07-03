0e745923002bd82c113daf859ae5576b1ea62827
Rajeshgupta123456789 <32204956+Rajeshgupta123456789@users.noreply.github.com>
2025-07-03 14:04:49 -0400

    Create output.tf

---
+++ terraform/phase2-eks/vpc/output.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+}
+
+output "security_group_id" {
+  value = aws_security_group.eks_nodes.id
+}
