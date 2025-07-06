7e0a376404783bd8c464b275b4d599ac1a021330
Ankit Gupta <32204956+Rajeshgupta123456789@users.noreply.github.com>
2025-07-05 23:59:07 -0400

    Update variable.tf

--- terraform/phase2-eks/vpc/variable.tf
+++ terraform/phase2-eks/vpc/variable.tf
@@ -2,3 +2,13 @@ variable "region" {
   description = "AWS region"
   type        = string
 }
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
