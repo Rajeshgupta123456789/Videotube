f89114e19c8abf2f7bbff453e674685a44f7aafe
Ankit Gupta <32204956+Rajeshgupta123456789@users.noreply.github.com>
2025-07-05 23:59:41 -0400

    Update output.tf

--- terraform/phase2-eks/vpc/output.tf
+++ terraform/phase2-eks/vpc/output.tf
@@ -9,3 +9,8 @@ output "public_subnet_ids" {
 output "security_group_id" {
   value = aws_security_group.eks_nodes.id
 }
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
