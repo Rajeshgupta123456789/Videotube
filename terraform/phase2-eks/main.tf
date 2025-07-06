00555e476d0538f08633e23ca3f4888325af1dac
Ankit Gupta <32204956+Rajeshgupta123456789@users.noreply.github.com>
2025-07-06 00:00:53 -0400

    Update main.tf

--- terraform/phase2-eks/main.tf
+++ terraform/phase2-eks/main.tf
@@ -17,6 +17,8 @@ provider "aws" {
 module "vpc" {
   source = "./vpc"
   region = var.region
+  private_subnet_cidrs = ["10.0.101.0/24", "10.0.102.0/24"]
+  availability_zones   = ["us-east-1a", "us-east-1b"]
 }
 
 module "iam" {
@@ -38,4 +40,12 @@ module "nodegroup" {
   ami_type       = "BOTTLEROCKET_x86_64"
   security_group_ids  = [module.vpc.security_group_id]
   key_name = "id_rsa"
+}
+
+module "rds" {
+  source              = "./rds"
+  vpc_id              = module.vpc.vpc_id
+  private_subnet_ids  = module.vpc.private_subnet_ids
+  db_name             = "videotube"
+  secret_arn          = "arn:aws:secretsmanager:us-east-1:208496905340:secret:videotube/rds/postgres-hTiQPt"
 }
