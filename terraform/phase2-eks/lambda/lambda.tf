71ab0f5629759b326318949f1b7935c3cfb19530
Ankit Gupta <32204956+Rajeshgupta123456789@users.noreply.github.com>
2025-07-07 14:32:47 -0400

    Create lambda.tf

---
+++ terraform/phase2-eks/lambda/lambda.tf
@@ -0,0 +1,73 @@
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_presigned_upload_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "lambda_presigned_upload_policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Effect   = "Allow",
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ],
+        Effect = "Allow",
+        Resource = [
+          "arn:aws:s3:::${var.s3_bucket_name}",
+          "arn:aws:s3:::${var.s3_bucket_name}/*"
+        ]
+      },
+      {
+        Action = "secretsmanager:GetSecretValue",
+        Effect = "Allow",
+        Resource = var.db_secret_arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+resource "aws_lambda_function" "presigned_upload" {
+  function_name = "generatePresignedURL"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.11"
+  timeout       = 10
+  filename      = "../lambda_function.zip" # optional if deploying manually
+
+  environment {
+    variables = {
+      S3_BUCKET_NAME = var.s3_bucket_name
+      DB_SECRET_ARN  = var.db_secret_arn
+      DB_HOST        = var.db_host
+      DB_NAME        = var.db_name
+    }
+  }
+}
