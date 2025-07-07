a8fa7a0fd2d4cd964a84358d2bfae37635863657
Ankit Gupta <32204956+Rajeshgupta123456789@users.noreply.github.com>
2025-07-07 14:30:09 -0400

    Update output.tf

--- terraform/phase2-eks/rds/output.tf
+++ terraform/phase2-eks/rds/output.tf
@@ -7,3 +7,13 @@ output "rds_security_group_id" {
   description = "RDS Security Group ID"
   value       = aws_security_group.rds_sg.id
 }
+
+output "secret_arn" {
+  description = "The ARN of the RDS credentials secret"
+  value       = var.secret_arn
+}
+
+output "db_endpoint" {
+  description = "The endpoint of the RDS instance"
+  value       = aws_db_instance.videotube_rds.endpoint
+}
