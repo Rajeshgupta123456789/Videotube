5326fd7e64584e575525b2c0ba3c4fbece0f666f
Ankit Gupta <32204956+Rajeshgupta123456789@users.noreply.github.com>
2025-07-07 15:56:08 -0400

    Update app.js

--- docker/backend/app.js
+++ docker/backend/app.js
@@ -2,10 +2,29 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const redis = require("./redisClient");
+
+
 app.get("/", (req, res) => {
-  res.send("Welcome to the VideoTube Backend API!");
+  res.send("Welcome to Videotube 🎥 ");
+});
+
+app.get("/cache-example", async (req, res) => {
+  try {
+    const cached = await redis.get("demo-key");
+    if (cached) {
+      return res.json({ from: "cache", data: JSON.parse(cached) });
+    }
+
+    const data = { message: "Fresh data from backend" };
+    await redis.set("demo-key", JSON.stringify(data), "EX", 3600); // 1hr
+    res.json({ from: "origin", data });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`🚀  Server running on port ${PORT}`);
 });
+
